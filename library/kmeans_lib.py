ad6857d3765096f27a0f431d689aeb66b3f3b109
tamara <tamarayustian@gmail.com>
2022-08-26 17:29:50 +0800

    clean up requirements

--- library/kmeans_lib.py
+++ library/kmeans_lib.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import warnings
+import seaborn as sns
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 
