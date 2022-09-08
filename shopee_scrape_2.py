c1229b318160e36e915a7fecfb5c858eaf41af08
tamara <tamarayustian@gmail.com>
2022-09-08 11:45:26 +0800

    add subcategory tier

--- shopee_scrape_2.py
+++ shopee_scrape_2.py
@@ -46,6 +46,20 @@ def main():
         shopee.download_images(csv_path, path)
         logging.info(f"Done downloading images for {keyword}")
 
+        for subcategory in subcategory_list:
+            if subcategory['parent_catid'] == category['catid']:
+                keyword_sub = subcategory['name']
+                print(f"Searching {keyword_sub}")
+                logging.info(f"Searching {keyword_sub}")
+                df_result_sub, json_result_sub = shopee.global_search(keyword_sub)
+
+                path_sub = shopee.create_folder(keyword_sub, f"data/{keyword}")
+                csv_path_sub = shopee.create_file(time_str + keyword_sub.replace(" ", "_"), "csv", path_sub)
+                json_path_sub = shopee.create_file(time_str + keyword_sub.replace(" ", "_"), "json", path_sub)
+                df_result_sub.to_csv(csv_path_sub)
+                shopee.to_json(json_path_sub, json_result_sub)
+                logging.info(f"{keyword_sub} saved in {path_sub}")
+
     # logs to mark ending time
     logging.info("Getting general information: done!")
     logging.info(f"--- {time.time() - start_time} seconds for general information ---")
