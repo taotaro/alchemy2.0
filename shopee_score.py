42aacee0c2bdaaa1b3a3bbe9f85baee91dab5e41
tamara <tamarayustian@gmail.com>
2022-09-22 17:57:18 +0800

    add cors

--- shopee_score.py
+++ shopee_score.py
@@ -1,11 +1,13 @@
 from cgi import test
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, request
+from flask_cors import CORS
 import pandas as pd
 import ast
 from library import score_lib
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class Check(Resource):
@@ -36,4 +38,10 @@ api.add_resource(Check, '/check') # entry point for Health Check
 api.add_resource(Score, '/score') # entry point for Score
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True, ssl_context=('/etc/letsencrypt/live/taotaroapp.com/fullchain.pem', '/etc/letsencrypt/live/taotaroapp.com/privkey.pem'))  # run our Flask app
+    app.run(
+      host="0.0.0.0", 
+      port=5000, 
+      debug=True, 
+      threaded=True, 
+      ssl_context=('/etc/letsencrypt/live/taotaroapp.com/fullchain.pem', '/etc/letsencrypt/live/taotaroapp.com/privkey.pem')
+    )  # run our Flask app
