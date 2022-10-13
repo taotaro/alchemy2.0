de35c8e854bb686630b19ca4619c2f3dc2dec4d8
tamara <tamarayustian@gmail.com>
2022-10-13 17:17:43 +0800

    bug fix

--- shopee_score.py
+++ shopee_score.py
@@ -97,9 +97,9 @@ def get_category():
 if __name__ == '__main__':
     init_db()
     app.run(
-      # host="0.0.0.0", 
+      host="0.0.0.0", 
       port=5000, 
       debug=True, 
       threaded=True, 
-      # ssl_context=('/etc/letsencrypt/live/taotaroapp.com/fullchain.pem', '/etc/letsencrypt/live/taotaroapp.com/privkey.pem')
+      ssl_context=('/etc/letsencrypt/live/taotaroapp.com/fullchain.pem', '/etc/letsencrypt/live/taotaroapp.com/privkey.pem')
     )  # run our Flask app
