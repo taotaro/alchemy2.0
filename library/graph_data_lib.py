68190e54e3574f06eb50c908f4c5f8efa8783090
kavyagb22 <kgbalaji@connect.ust.hk>
2022-10-25 12:26:23 +0800

    graph data api created

--- library/graph_data_lib.py
+++ library/graph_data_lib.py
@@ -11,22 +11,20 @@ def get_data(uri, folder, file):
 
 
 def get_data_for_category(data, category):
+  print('looking')
   category_data = []
-  count = 10
   for i in data:
     if i['cat_name'] == category:
      if 'item_rating' and 'discount' in i['product']:
-       category_data.append(i)
+      category_data.append(i)
+  print('done')
   return category_data
-
-
-
 
 def get_data_for_scatter_plot(data):
   data_updated = []
   for i in data:
     new_data = {}
-    new_data['x'] = i['product']['item_rating']['rating_star']
+    new_data['x'] = round(i['product']['item_rating']['rating_star'], 2)
     new_data['y'] = i['product']['sold']
     data_updated.append(new_data)
   json_data = json.dumps(data_updated)
@@ -39,10 +37,7 @@ def get_data_for_bar_graph(data):
     new_data['product'] = i['product']['name'][0:20]
     new_data['sales'] = i['product']['sold']
     new_data['historicalSales'] = i['product']['historical_sold']
-    if i['product']['discount'] is not None:
-      new_data['discount'] =  i['product']['discount'].strip('%')
-    else:
-      new_data['discount'] = 0
+    new_data['discount'] =  int(str(i['product']['discount']).strip('%'))
     new_data['ratingCount'] = i['product']['item_rating']['rating_count']
     new_data['likesCount'] = i['product']['liked_count']
     data_updated.append(new_data)
@@ -56,10 +51,9 @@ def get_data_for_swarm_graph(data):
     new_data['id'] = i['product']['itemid']
     new_data['group'] = str(i['product']['shopee_verified'])
     new_data['data'] = i['product']['sold']
-    if i['product']['discount'] is not None:
-      new_data['discount'] =  i['product']['discount'].strip('%')
-    else:
-      new_data['discount'] = 0
+   
+    new_data['discount'] =  int(str(i['product']['discount']).strip('%'))
+   
     data_updated.append(new_data)
   json_data = json.dumps(data_updated)
   return json_data
@@ -74,9 +68,20 @@ def get_data_for_bar_revenue(data):
   json_data = json.dumps(data_updated)
   return json_data
 
-def get_data_for_pie_chart(data, position, id, label):
+def get_data_for_pie_chart(data, name):
   data_updated = []
-  user_data = data[position]['product']['item_rating']['rating_count']
+  user_data = []
+  label = [
+    '1 star', '2 star', '3 star', '4 star', '5 star'
+  ]
+
+  id = ["*", "**", "***", "****", "*****"]
+
+  for i in data:
+    if i['product']['name'] == name:
+      user_data = i['product']['item_rating']['rating_count']
+
+
   for i in range(len(user_data)):
     new_data={}
     if i==0:
@@ -89,33 +94,55 @@ def get_data_for_pie_chart(data, position, id, label):
   json_data = json.dumps(data_updated)
   return json_data
 
+def get_graph_data(client, category, product_name):
+  data = get_data(client, 'alchemy', 'sProducts')
+  category_data = get_data_for_category(data.find(), category)
+
+  for i in category_data:
+    for j in i['product']:
+      if i['product'][j] is None:
+        i['product'].update({j:0})
+      else:
+        continue
+
+  
+
+  scatter_data = get_data_for_scatter_plot(category_data)
+  bar_data = get_data_for_bar_graph(category_data)
+  swarm_data = get_data_for_swarm_graph(category_data)
+  revenue_data = get_data_for_bar_revenue(category_data)
+  pie_data = get_data_for_pie_chart(category_data, product_name)
+
+  return scatter_data, bar_data, swarm_data, revenue_data, pie_data
+  
+
+
 
 
 if __name__ == "__main__":
 
   client = "mongodb://root:1000Sunny@externaltaotarodb1.mongodb.rds.aliyuncs.com:3717,externaltaotarodb2.mongodb.rds.aliyuncs.com:3717/admin?authSource=admin&replicaSet=mgset-28314303&readPreference=primary&ssl=false"
-  data = get_data(client, 'alchemy', 'sProducts')
-  category_data = get_data_for_category(data.find(), 'Tops')
+  test_product = 'Athena Crop Top'
+  test_scatter, test_bar, test_swarm, test_revenue, test_pie=get_graph_data(client, 'Tops', test_product)
+  print(test_scatter)
 
-  print('category len: ', len(category_data))
-  scatter_data = get_data_for_scatter_plot(category_data)
-  print(scatter_data)
-  bar_data = get_data_for_bar_graph(category_data)
-  print(bar_data)
-  swarm_data = get_data_for_swarm_graph(category_data)
-  print(swarm_data)
-  revenue_data = get_data_for_bar_revenue(category_data)
-  print(revenue_data)
-  pieLabels = [
-    ("ShopeeGraph.dashboard.pieChart.labels.oneStar"),
-    ("ShopeeGraph.dashboard.pieChart.labels.twoStar"),
-    ("ShopeeGraph.dashboard.pieChart.labels.threeStar"),
-    ("ShopeeGraph.dashboard.pieChart.labels.fourStar"),
-    ("ShopeeGraph.dashboard.pieChart.labels.fiveStar"),
-  ]
 
-  pieIds = ["*", "**", "***", "****", "*****"]
+  # data = get_data(client, 'alchemy', 'sProducts')
+  # category_data = get_data_for_category(data.find(), 'Tops')
 
-  pie_data = get_data_for_pie_chart(category_data, 2, pieIds, pieLabels)
-  print(pie_data)
+  # print('category len: ', len(category_data))
+  # scatter_data = get_data_for_scatter_plot(category_data)
+  # print(scatter_data)
+  # bar_data = get_data_for_bar_graph(category_data)
+  # print(bar_data)
+  # swarm_data = get_data_for_swarm_graph(category_data)
+  # print(swarm_data)
+  # revenue_data = get_data_for_bar_revenue(category_data)
+  # print(revenue_data)
+  
+  # test_product = 'Athena Crop Top'
+  
+
+  # pie_data = get_data_for_pie_chart(category_data, test_product)
+  # print(pie_data)
     
